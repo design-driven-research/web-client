6a4cdc59a8d5285b6f18fa3211d838dddb6ab5ae
Aram Zadikian <brancusi@gmail.com>
2021-09-04 19:28:29 -0700

    wip

--- src/rdd/core.cljs
+++ src/rdd/core.cljs
@@ -1,64 +1,26 @@
 (ns rdd.core
-  (:require [re-frame.core :as rf]
-            [rdd.events :as evt]
-            [datascript.transit :as dt]
-            [datascript.core :as d]
-            [reagent.core :as reagent :refer [atom]]
-            [reagent.dom :as rdom]
-            ;; [db :refer [conn]]
-            [rdd.views :as views]
-            [rdd.syncer]
-            [taoensso.timbre :as timbre
-             :refer-macros [info]]))
+  (:require [helix.core :refer [defnc $]]
+            [helix.hooks :as hooks]
+            [helix.dom :as d]
+            ["react-dom" :as rdom]))
 
-(defn dev-setup []
-  (when true
-    (info "dev mode")))
+;; define components using the `defnc` macro
+(defnc greeting
+  "A component which greets a user."
+  [{:keys [name]}]
+  ;; use helix.dom to create DOM elements
+  (d/div "Hello, " (d/strong name) "!"))
 
-(defn ^:dev/after-load mount-root []
+(defnc app []
+  (let [[state set-state] (hooks/use-state {:name "Helix User"})]
+    (d/div
+     (d/h1 "Welcome!")
+      ;; create elements out of components
+     ($ greeting {:name (:name state)})
+     (d/input {:value (:name state)
+               :on-change #(set-state assoc :name (.. % -target -value))}))))
 
-  ;; Restart DB on each save. Disable if you want to retain state
-  ;; across saves
-  ;; (rf/dispatch-sync [::events/initialize-db])
-  ;; (re-posh/dispatch-sync [::evt/initialize-db])
-
-  (rf/clear-subscription-cache!)
-
-  (let [root-el (.getElementById js/document "app")]
-    (rdom/unmount-component-at-node root-el)
-    (rdom/render [views/main-panel] root-el)))
-
-(defn init! []
-  (dev-setup)
-  (rf/dispatch-sync [::evt/initialize-db])
-  #_(let [stored (js/localStorage.getItem "datascript/db")]
-      (if stored
-
-        #_(reset! conn (dt/read-transit-str stored))
-        (re-posh/dispatch-sync [::evt/initialize-db])
-        #_(re-posh/dispatch-sync [::evt/initialize-db])))
-
-  (mount-root))
-
-;; (comment
-
-;;   (->> (js/localStorage.getItem "datascript/db")
-;;        (dt/read-transit-str)
-;;        (reset! conn))
-;;   ;; => #datascript/DB {:schema nil, :datoms [[1 :app/type :type/create-todo-form 536870913] [1 :create-todo-form/description "" 536870913] [1 :create-todo-form/title "" 536870913] [2 :app/type :type/task 536870913] [2 :task/description "Just learn it" 536870913] [2 :task/done? false 536870913] [2 :task/title "Learn Clojure a little bit" 536870913] [3 :app/type :type/task 536870913] [3 :task/description "Just relax" 536870913] [3 :task/done? false 536870913] [3 :task/title "Have a coffe" 536870913]]}
-
-
-;;   @conn
-;;   ;; => #datascript/DB {:schema nil, :datoms [[1 :app/type :type/create-todo-form 536870913] [1 :create-todo-form/description "" 536870913] [1 :create-todo-form/title "" 536870913] [2 :app/type :type/task 536870913] [2 :task/description "Just learn it" 536870913] [2 :task/done? false 536870913] [2 :task/title "Learn Clojure a little bit" 536870913] [3 :app/type :type/task 536870913] [3 :task/description "Just relax" 536870913] [3 :task/done? false 536870913] [3 :task/title "Have a coffe" 536870913]]}
-
-;;   ;; => #datascript/DB {:schema nil, :datoms [[1 :app/type :type/create-todo-form 536870913] [1 :create-todo-form/description "" 536870913] [1 :create-todo-form/title "" 536870913] [2 :app/type :type/task 536870913] [2 :task/description "Just learn it" 536870913] [2 :task/done? false 536870913] [2 :task/title "Learn Clojure a little bit" 536870913] [3 :app/type :type/task 536870913] [3 :task/description "Just relax" 536870913] [3 :task/done? false 536870913] [3 :task/title "Have a coffe" 536870913]]}
-
-;;   ;; => #datascript/DB {:schema nil, :datoms [[1 :app/type :type/create-todo-form 536870913] [1 :create-todo-form/description "" 536870913] [1 :create-todo-form/title "" 536870913] [2 :app/type :type/task 536870913] [2 :task/description "Just learn it" 536870913] [2 :task/done? false 536870913] [2 :task/title "Learn Clojure a little bit" 536870913] [3 :app/type :type/task 536870913] [3 :task/description "Just relax" 536870913] [3 :task/done? false 536870913] [3 :task/title "Have a coffe" 536870913]]}
-
-;;   ;; => #datascript/DB {:schema nil, :datoms [[1 :app/type :type/create-todo-form 536870913] [1 :create-todo-form/description "" 536870913] [1 :create-todo-form/title "" 536870913] [2 :app/type :type/task 536870913] [2 :task/description "Just learn it" 536870913] [2 :task/done? false 536870913] [2 :task/title "Learn Clojure a little bit" 536870913] [3 :app/type :type/task 536870913] [3 :task/description "Just relax" 536870913] [3 :task/done? false 536870913] [3 :task/title "Have a coffe" 536870913]]}
-
-
-
-
-;;   ;; 
-;;   )
+;; start your app with your favorite React renderer
+(defn init!
+  []
+  (rdom/render ($ app) (js/document.getElementById "app")))
